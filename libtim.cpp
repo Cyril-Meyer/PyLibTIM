0a5cc03af29bcb9bc3a260b910e97ae5b88c9e23
Cyril Meyer <contact@cyrilmeyer.eu>
2022-02-07 14:28:54 +0100

    work on attribute image

--- libtim.cpp
+++ libtim.cpp
@@ -1,68 +1,96 @@
-#include <iostream>
 #include <ctime>
+#include <iostream>
 
-#include "libtim/Common/Image.h"
-#include "libtim/Common/FlatSE.h"
 #include "libtim/Algorithms/ComponentTree.h"
 #include "libtim/Algorithms/ConnectedComponents.h"
 #include "libtim/Algorithms/Thresholding.h"
+#include "libtim/Common/FlatSE.h"
+#include "libtim/Common/Image.h"
 
 using namespace std;
 using namespace LibTIM;
 
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-FlatSE getFlatSE(int connexity_id)
-{
-	FlatSE connexity;
-	switch(connexity_id)
-	{
-		case 1:
-		connexity.make2DN4();
-		break;
-		case 2:
-		connexity.make2DN8();
-		break;
-		case 3:
-		connexity.make3DN6();
-		break;
-		case 4:
-		connexity.make3DN18();
-		break;
-		case 5:
-		connexity.make3DN26();
-		break;
-		default:
-		connexity.make2DN8();
-		break;
-	}
-	return connexity;
+enum ConnexityID { C2DN4, C2DN8, C3DN6, C3DN18, C3DN26 };
+enum AttributeID {
+  AREA,
+  CONTRAST,
+  VOLUME,
+  CONTOUR_LENGTH,
+  COMPLEXITY,
+  COMPACITY,
+  HU_INVARIANT_MOMENT
+};
+enum AttributeValID { NODE, MAX_PARENTS, MIN_PARENTS };
+
+FlatSE getFlatSE(int connexity_id) {
+  FlatSE connexity;
+  switch (connexity_id) {
+    case C2DN4:
+      connexity.make2DN4();
+      break;
+    case C2DN8:
+      connexity.make2DN8();
+      break;
+    case C3DN6:
+      connexity.make3DN6();
+      break;
+    case C3DN18:
+      connexity.make3DN18();
+      break;
+    case C3DN26:
+      connexity.make3DN26();
+      break;
+    default:
+      connexity.make2DN8();
+      break;
+  }
+  return connexity;
 }
 
-void area_filtering(int x, int y, int z, py::array_t<uint8_t> image, int area, int connexity_id)
-{
-	auto r = image.mutable_unchecked<3>();
+int getAttribute(Node *n, AttributeID attribute_id) {
+  switch (attribute_id) {
+    case AREA:
+      return (int)n->area;
+    case CONTRAST:
+      return (int)n->contrast;
+    case VOLUME:
+      return (int)n->volume;
+    case CONTOUR_LENGTH:
+      return (int)n->contourLength;
+    case COMPLEXITY:
+      return (int)n->complexity;
+    case COMPACITY:
+      return (int)n->compacity;
+    case HU_INVARIANT_MOMENT:
+        return (int)n->I;
+  }
+  return 0;
+}
+
+void area_filtering(int x, int y, int z, py::array_t<uint8_t> image, int area,
+                    int connexity_id) {
+  auto r = image.mutable_unchecked<3>();
 
   // Image
   // TSize size[] = {x, y, z};
   // TSpacing spacing[] = {1.0, 1.0, 1.0};
   // const U8 data[] = {0, 1, 2, 3, 4, ...}
-	TSize size[] = {(short unsigned int)x,
-								  (short unsigned int)y,
-									(short unsigned int)z};
+  TSize size[] = {(short unsigned int)x, (short unsigned int)y,
+                  (short unsigned int)z};
   Image<U8> im(size);
 
-	for (py::ssize_t i = 0; i < r.shape(0); i++)
-		for (py::ssize_t j = 0; j < r.shape(1); j++)
-			for (py::ssize_t k = 0; k < r.shape(2); k++)
-				im(i, j, k) = r(i, j, k);
+  for (py::ssize_t i = 0; i < r.shape(0); i++)
+    for (py::ssize_t j = 0; j < r.shape(1); j++)
+      for (py::ssize_t k = 0; k < r.shape(2); k++) im(i, j, k) = r(i, j, k);
 
   // Construction du component-tree
-	FlatSE connexity = getFlatSE(connexity_id);
-	ComponentTree<U8> tree(im, connexity);
+  FlatSE connexity = getFlatSE(connexity_id);
+  ComponentTree<U8> tree(im, connexity);
 
   // Elagage de l'arbre
   tree.areaFiltering(area);
@@ -70,61 +98,89 @@ void area_filtering(int x, int y, int z, py::array_t<uint8_t> image, int area, i
   // Reconstruction d'une image à partir de l'arbre élagué
   Image<U8> res = tree.constructImage(ComponentTree<U8>::DIRECT);
 
-	for (py::ssize_t i = 0; i < r.shape(0); i++)
-		for (py::ssize_t j = 0; j < r.shape(1); j++)
-			for (py::ssize_t k = 0; k < r.shape(2); k++)
-				r(i, j, k) = (uint8_t)res(i, j, k);
+  for (py::ssize_t i = 0; i < r.shape(0); i++)
+    for (py::ssize_t j = 0; j < r.shape(1); j++)
+      for (py::ssize_t k = 0; k < r.shape(2); k++)
+        r(i, j, k) = (uint8_t)res(i, j, k);
 }
 
-void attribute_image(int x, int y, int z, py::array_t<uint8_t> image, py::array_t<int> image_attr, int connexity_id)
-{
-	auto r = image.mutable_unchecked<3>();
-	auto r_attr = image_attr.mutable_unchecked<4>();
+void attribute_image(int x, int y, int z, py::array_t<uint8_t> image,
+                     py::array_t<int> image_attr, ConnexityID connexity_id,
+                     AttributeID attribute_id,
+                     AttributeValID attribute_val_id) {
+  auto r = image.mutable_unchecked<3>();
+  auto r_attr = image_attr.mutable_unchecked<3>();
 
   // Image
   // TSize size[] = {x, y, z};
   // TSpacing spacing[] = {1.0, 1.0, 1.0};
   // const U8 data[] = {0, 1, 2, 3, 4, ...}
-	TSize size[] = {(short unsigned int)x,
-								  (short unsigned int)y,
-									(short unsigned int)z};
+  TSize size[] = {(short unsigned int)x, (short unsigned int)y,
+                  (short unsigned int)z};
   Image<U8> im(size);
 
-	for (py::ssize_t i = 0; i < r.shape(0); i++)
-		for (py::ssize_t j = 0; j < r.shape(1); j++)
-			for (py::ssize_t k = 0; k < r.shape(2); k++)
-				im(i, j, k) = r(i, j, k);
+  for (py::ssize_t i = 0; i < r.shape(0); i++)
+    for (py::ssize_t j = 0; j < r.shape(1); j++)
+      for (py::ssize_t k = 0; k < r.shape(2); k++) im(i, j, k) = r(i, j, k);
 
   // Construction du component-tree
-	FlatSE connexity = getFlatSE(connexity_id);
-	ComponentTree<U8> tree(im, connexity);
+  FlatSE connexity = getFlatSE(connexity_id);
+  ComponentTree<U8> tree(im, connexity);
 
-	// Reconstruction d'une image à partir de la valeur des attributs
-	// construction de la liste des noeuds
-	std::vector<Node *> nodes = tree.indexedNodes();
-	for (py::ssize_t i = 0; i < r.shape(0); i++)
-		for (py::ssize_t j = 0; j < r.shape(1); j++)
-			for (py::ssize_t k = 0; k < r.shape(2); k++)
-				{
-					/*
-					r_attr(i, j, k, 0) = tree.indexedCoordToNode(i, j, k, nodes)->area;
-					r_attr(i, j, k, 1) = tree.indexedCoordToNode(i, j, k, nodes)->volume;
-					r_attr(i, j, k, 2) = tree.indexedCoordToNode(i, j, k, nodes)->contrast;
-					r_attr(i, j, k, 3) = tree.indexedCoordToNode(i, j, k, nodes)->contourLength;
-					r_attr(i, j, k, 4) = tree.indexedCoordToNode(i, j, k, nodes)->complexity;
-					r_attr(i, j, k, 5) = tree.indexedCoordToNode(i, j, k, nodes)->compacity;
-					r_attr(i, j, k, 6) = tree.indexedCoordToNode(i, j, k, nodes)->subNodes;
-					*/
-					r_attr(i, j, k, 0) = tree.indexedCoordToNode(i, j, k, nodes)->area;
-					r_attr(i, j, k, 1) = tree.indexedCoordToNode(i, j, k, nodes)->contrast;
-					r_attr(i, j, k, 2) = tree.indexedCoordToNode(i, j, k, nodes)->contourLength;
-					r_attr(i, j, k, 3) = tree.indexedCoordToNode(i, j, k, nodes)->subNodes;
-				}
+  // Reconstruction d'une image à partir de la valeur des attributs
+  // construction de la liste des noeuds
+  std::vector<Node *> nodes = tree.indexedNodes();
+  for (py::ssize_t i = 0; i < r.shape(0); i++)
+    for (py::ssize_t j = 0; j < r.shape(1); j++)
+      for (py::ssize_t k = 0; k < r.shape(2); k++) {
+        Node *n = tree.indexedCoordToNode(i, j, k, nodes);
+        int attr = getAttribute(n, attribute_id);
+
+        if (attribute_val_id == NODE) {
+          // nothing.
+        } else if (attribute_val_id == MAX_PARENTS) {
+          while (n->father != tree.m_root) {
+            n = n->father;
+            attr = std::max(attr, getAttribute(n, attribute_id));
+          }
+          attr = std::max(attr, getAttribute(n, attribute_id));
+        } else if (attribute_val_id == MIN_PARENTS) {
+          while (n->father != tree.m_root) {
+            n = n->father;
+            attr = std::min(attr, getAttribute(n, attribute_id));
+          }
+          attr = std::min(attr, getAttribute(n, attribute_id));
+        }
+        r_attr(i, j, k) = attr;
+      }
 }
 
-PYBIND11_MODULE(libtim, m)
-{
+PYBIND11_MODULE(libtim, m) {
   m.doc() = "libtim";
   m.def("area_filtering", &area_filtering);
   m.def("attribute_image", &attribute_image);
+
+  py::enum_<ConnexityID>(m, "ConnexityID")
+      .value("C2DN4", C2DN4)
+      .value("C2DN8", C2DN8)
+      .value("C3DN6", C3DN6)
+      .value("C3DN18", C3DN18)
+      .value("C3DN26", C3DN26)
+      .export_values();
+
+  py::enum_<AttributeID>(m, "AttributeID")
+      .value("AREA", AREA)
+      .value("CONTRAST", CONTRAST)
+      .value("VOLUME", VOLUME)
+      .value("CONTOUR_LENGTH", CONTOUR_LENGTH)
+      .value("COMPLEXITY", COMPLEXITY)
+      .value("COMPACITY", COMPACITY)
+      .value("HU_INVARIANT_MOMENT", HU_INVARIANT_MOMENT)
+      .export_values();
+
+  py::enum_<AttributeValID>(m, "AttributeValID")
+      .value("NODE", NODE)
+      .value("MAX_PARENTS", MAX_PARENTS)
+      .value("MIN_PARENTS", MIN_PARENTS)
+      .export_values();
 }
--- test.py
+++ test.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 import tifffile
 
 import pylibtim.libtim as tim
-import pylibtim.utils as tim_utils
 
 # VREMS-data used for test https://git.unistra.fr/cymeyer/VREMS-data
-# IMG = np.array(tifffile.imread('VREMS-data/lucchi/image.tif'), dtype=np.uint8)
+IMG = np.array(tifffile.imread('VREMS-data/lucchi/image.tif'), dtype=np.uint8)
 # Smaller for faster test
-# IMG = IMG[0:32, 0:512, 0:512]
+IMG = IMG[0:32, 0:256, 0:256]
+z = 16
 
 # IMG = np.array(tifffile.imread('JG1/ROI2/ROI2.tif'), dtype=np.uint8)
 # z = 180
@@ -19,9 +19,9 @@ import pylibtim.utils as tim_utils
 # IMG = IMG[0:150]
 # z = 45
 
-IMG = np.array(tifffile.imread('I3/i3.tif'), dtype=np.uint8)
-IMG = IMG[0:20]
-z = 10
+# IMG = np.array(tifffile.imread('I3/i3.tif'), dtype=np.uint8)
+# IMG = IMG[0:20]
+# z = 10
 
 print(IMG.shape, IMG.dtype, IMG.min(), IMG.max())
 
@@ -37,7 +37,7 @@ if TEST_AREA_FILTERING:
 
     I = np.swapaxes(np.swapaxes(np.copy(IMG), 0,-1), 0, 1)[:, :, z:z+1]
     S = I.shape
-    connexity = tim_utils.FlatSE('make2DN8')
+    connexity = tim.C2DN8
     tim.area_filtering(S[0], S[1], S[2], I, 10000, connexity)
 
     print(I.shape, I.dtype, I.min(), I.max())
@@ -46,7 +46,7 @@ if TEST_AREA_FILTERING:
 
     I = np.copy(IMG)
     S = I.shape
-    connexity = tim_utils.FlatSE('make3DN26')
+    connexity = tim.C3DN26
     t0 = time.time()
     tim.area_filtering(S[0], S[1], S[2], I, 100000, connexity)
     t1 = time.time()
@@ -58,7 +58,7 @@ if TEST_AREA_FILTERING:
 
     I = np.copy(IMG)
     S = I.shape
-    connexity = tim_utils.FlatSE('make3DN26')
+    connexity = tim.C3DN26
     t0 = time.time()
     tim.area_filtering(S[0], S[1], S[2], I, 1000000, connexity)
     t1 = time.time()
@@ -72,31 +72,59 @@ if TEST_AREA_FILTERING:
 
 
 if TEST_ATTRIBUTE_IMAGE:
-    fig, axs = plt.subplots(2, 3)
+    fig, axs = plt.subplots(2, 2)
 
     I = np.copy(IMG)
-    I_ATTR = np.zeros((IMG.shape + (7,)), dtype=np.int32)
     S = I.shape
-    connexity = tim_utils.FlatSE('make3DN26')
-    t0 = time.time()
-    tim.attribute_image(S[0], S[1], S[2], I, I_ATTR, connexity)
-    t1 = time.time()
 
+    print(IMG.shape, IMG.dtype, IMG.min(), IMG.max())
+    axs[0, 0].imshow(IMG[z], cmap='gray')
+    axs[0, 0].set_title('original image')
+
+    AttributeID = tim.HU_INVARIANT_MOMENT
+
+    I_ATTR = np.zeros(IMG.shape, dtype=np.int32)
+    t0 = time.time()
+    tim.attribute_image(S[0], S[1], S[2], I, I_ATTR, tim.C3DN26, AttributeID, tim.NODE)
+    t1 = time.time()
     print(t1-t0)
-    print(I_ATTR[:, :, :, 0].shape, I_ATTR[:, :, :, 0].dtype, I_ATTR[:, :, :, 0].min(), I_ATTR[:, :, :, 0].max())
-    axs[0, 0].imshow(I_ATTR[z, :, :, 0]/I_ATTR[:,:,:,0].max(), cmap='gray')
-    axs[0, 0].set_title('3D image - attribute_image area')
-    print(I_ATTR[:, :, :, 1].shape, I_ATTR[:, :, :, 1].dtype, I_ATTR[:, :, :, 1].min(), I_ATTR[:, :, :, 1].max())
-    axs[0, 1].imshow(I_ATTR[z, :, :, 1]/I_ATTR[:,:,:,1].max(), cmap='gray')
-    axs[0, 1].set_title('3D image - attribute_image contrast')
-    print(I_ATTR[:, :, :, 2].shape, I_ATTR[:, :, :, 2].dtype, I_ATTR[:, :, :, 2].min(), I_ATTR[:, :, :, 2].max())
-    axs[0, 2].imshow(I_ATTR[z, :, :, 2]/I_ATTR[:,:,:,2].max(), cmap='gray')
-    axs[0, 2].set_title('3D image - attribute_image contourLength')
-    print(I_ATTR[:, :, :, 3].shape, I_ATTR[:, :, :, 3].dtype, I_ATTR[:, :, :, 3].min(), I_ATTR[:, :, :, 3].max())
-    axs[1, 0].imshow(I_ATTR[z, :, :, 3]/I_ATTR[:,:,:,3].max(), cmap='gray')
-    axs[1, 0].set_title('3D image - attribute_image subNodes')
-    print(I.shape, I.dtype, I.min(), I.max())
-    axs[1, 2].imshow(I[z, :, :], cmap='gray')
-    axs[1, 2].set_title('3D image - original')
+
+    print(np.histogram(I_ATTR))
+
+    print(I_ATTR.shape, I_ATTR.dtype, I_ATTR.min(), I_ATTR.max())
+    # I_ATTR = I_ATTR / I_ATTR.max()
+    axs[0, 1].imshow(I_ATTR[z], cmap='gray')
+    axs[0, 1].set_title('attribute image node')
+
+    I_ATTR_MAX = np.zeros(IMG.shape, dtype=np.int32)
+    t0 = time.time()
+    tim.attribute_image(S[0], S[1], S[2], I, I_ATTR_MAX, tim.C3DN26, AttributeID, tim.MAX_PARENTS)
+    t1 = time.time()
+    print(t1-t0)
+
+    print(I_ATTR_MAX.shape, I_ATTR_MAX.dtype, I_ATTR_MAX.min(), I_ATTR_MAX.max())
+    # I_ATTR_MAX = I_ATTR_MAX / I_ATTR_MAX.max()
+    axs[1, 0].imshow(I_ATTR_MAX[z], cmap='gray')
+    axs[1, 0].set_title('attribute image max')
+
+    I_ATTR_MIN = np.zeros(IMG.shape, dtype=np.int32)
+    t0 = time.time()
+    tim.attribute_image(S[0], S[1], S[2], I, I_ATTR_MIN, tim.C3DN26, AttributeID, tim.MIN_PARENTS)
+    t1 = time.time()
+    print(t1-t0)
+
+    print(I_ATTR_MIN.shape, I_ATTR_MIN.dtype, I_ATTR_MIN.min(), I_ATTR_MIN.max())
+    I_ATTR_MIN = I_ATTR_MIN / I_ATTR_MIN.max()
+    axs[1, 1].imshow(I_ATTR_MIN[z], cmap='gray')
+    axs[1, 1].set_title('attribute image min')
+
+    '''
+    I_ATTR_DIFF = I_ATTR_MAX - I_ATTR
+    print(I_ATTR_DIFF.shape, I_ATTR_DIFF.dtype, I_ATTR_DIFF.min(), I_ATTR_DIFF.max())
+    axs[1, 1].imshow(I_ATTR_DIFF[z], cmap='gray')
+    axs[1, 1].set_title('attribute image diff')
+    '''
+
+    tifffile.imsave('image_pp.tif', I_ATTR_MAX)
 
     plt.show()
